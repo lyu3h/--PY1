6dcf7f173e353acedd18bb0cedf755cfa4653670
lyu3h <116177329+lyu3h@users.noreply.github.com>
2022-10-31 00:30:25 +0800

    лабораторная работа4

---
+++ лабораторная работа4/task2.py
@@ -0,0 +1,16 @@
+sentence = ['0', '0', '1', '2',]
+
+print("默认为 index=-1，删除最后一个列表值：", sentence.pop(-1), "\n")
+print("删除第一个元素：", sentence.pop(0), "\n")
+print("输出剩余元素：", sentence)
+
+sentence = ['0', '1', '1', '2','3']
+print("默认为 index=-1，删除最后一个列表值：", sentence.pop(-1), "\n")
+print("删除第一个元素：", sentence.pop(1), "\n")
+print("输出剩余元素：", sentence)
+
+sentence = ['0', '1', '2', '3','4','4']
+print("默认为 index=-1，删除最后一个列表值：", sentence.pop(-1), "\n")
+print("删除第一个元素：", sentence.pop(4), "\n")
+print("输出剩余元素：", sentence)
+
